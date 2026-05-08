bd49cf407b8122aa950dcd83e483a0232916df87
JohannesL2 <johannes.lindgren2002@gmail.com>
2026-05-08 22:17:54 +0200

    Initiate Lightswind UI and added cursor effect on login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
 import { AuroraShaders } from "@/components/ui/aurora"
 import { LoginForm } from "@/components/login-form"
+import SmokeyCursor from "@/components/lightswind/smokey-cursor"
 
 export default function Page() {
   return (
     <div className="relative flex min-h-svh w-screen items-center justify-center p-6 md:p-10 bg-neutral-950 overflow-hidden">
+      <SmokeyCursor
+        splatRadius={0.1}
+        splatForce={3000}
+        densityDissipation={8}
+        velocityDissipation={5}
+        colorUpdateSpeed={5}
+        className="z-1"
+      />
     <AuroraShaders 
-        className="fixed inset-0 h-screen w-screen z-0" 
+        className="fixed inset-0 h-screen w-screen z-1" 
         speed={0.2} 
         intensity={0.4}
         vibrancy={0.8}
@@ -13,7 +22,7 @@ export default function Page() {
         stretch={6.0}
       />
 
-      <div className="relative z-10 w-full max-w-sm">
+      <div className="relative z-9999 w-full max-w-sm">
           <LoginForm />
       </div>
     </div>
